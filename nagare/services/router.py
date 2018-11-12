de27730596feabadd48fb51a718fa5ca38ace7f6
Alain Poirier <alain.poirier@net-ng.com>
2018-11-12 16:50:56 +0100

    Initial checkin

---
+++ nagare/services/router.py
@@ -0,0 +1,65 @@
+# Encoding: utf-8
+
+# --
+# Copyright (c) 2008-2018 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+import routes
+from webob import exc
+from nagare.services import plugin
+
+
+class Router(plugin.Plugin):
+    """A service wrapped around the router
+
+    Being now a service, the router can be injected
+    """
+    @staticmethod
+    def create_dispatch_args(request, response, **params):
+        return request.path_info, request.method, request, response
+
+    def __call__(self, o, url, method, request, response):
+        mapper = getattr(o, 'nagare_urls_mapper', None)
+        if mapper is None:
+            raise exc.HTTPNotFound()
+
+        match_dict = mapper.match('/' + url.strip('/'), {
+            'REQUEST_METHOD': method,
+            'request': request
+        })
+
+        if match_dict is None:
+            raise exc.HTTPNotFound()
+
+        return match_dict.pop('action')(o, url, method, request, response, **match_dict)
+
+
+def route_for(cls, url='', methods=('GET', 'HEAD'), validate=None, **kw):
+    """The routing decorator"""
+
+    def _(f):
+        cls.nagare_urls_mapper = getattr(cls, 'nagare_urls_mapper', None) or routes.Mapper(register=False)
+
+        conditions = {}
+        if methods:
+            conditions['method'] = [methods] if isinstance(methods, (str, type(u''))) else methods
+
+        if validate:
+            conditions['function'] = lambda params, match_dict: validate(params['request'], match_dict)
+
+        cls.nagare_urls_mapper.connect(
+            None,
+            '/' + url.strip('/'),
+            action=f,
+            conditions=conditions,
+            **kw
+        )
+
+        return f
+
+    return _
---
+++ setup.py
@@ -0,0 +1,40 @@
+# Encoding: utf-8
+
+# --
+# Copyright (c) 2008-2018 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+import os
+
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as description:
+    LONG_DESCRIPTION = description.read()
+
+setup(
+    name='nagare-services-router',
+    author='Net-ng',
+    author_email='alain.poirier@net-ng.com',
+    description='Nagare REST urls router',
+    long_description=LONG_DESCRIPTION,
+    license='BSD',
+    keywords='',
+    url='https://github.com/nagareproject/services-router',
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    install_requires=[
+        'webob', 'routes',
+        'nagare-services', 'nagare-server-http'
+    ],
+    entry_points='''
+        [nagare.services]
+        router = nagare.services.router:Router
+    '''
+)
